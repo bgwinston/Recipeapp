bef23d02943fd1d8ff7314218cdbfb2ffeabb6e6
bgwinston <b.winston87@yahoo.com>
2021-07-08 19:56:41 -0500

    Binding of library templates and recipe controller. Next create and connect models.

--- src/main/java/com/projects/Recipe/controllers/recipeController.java
+++ src/main/java/com/projects/Recipe/controllers/recipeController.java
@@ -2,17 +2,38 @@ package com.projects.Recipe.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping("library")
 public class recipeController {
+    static ArrayList<String> recipes = new ArrayList<>();
 
     @RequestMapping(value="myLibrary")
-    public String library(){
+    public String library(Model model){
+        model.addAttribute("title", "My Recipe Library");
+        model.addAttribute("recipes", recipes);
         return "library/myLibrary";
     }
-
+    public String remove(){
+        return "myLibrary";
+    }
+    @RequestMapping(value="add", method= RequestMethod.GET)
+    public String displayAdd(Model model){
+        model.addAttribute("title", "Add a recipe!");
+        return "library/add";
+    }
+    @RequestMapping(value="add", method=RequestMethod.POST)
+    public String processAdd(@RequestParam String recipeName) {
+        recipes.add(recipeName);
+        return "library/recipeConfirmation";
+    }
 }
 
 
---
+++ src/main/java/com/projects/Recipe/models/recipes.java
@@ -0,0 +1,47 @@
+package com.projects.Recipe.models;
+
+public class recipes {
+    private String name;
+    private String description;
+    private String category;
+    private String rating;
+
+    public recipes(String name, String description, String category, String rating) {
+        this.name=name;
+        this.description=description;
+        this.category=category;
+        this.rating=rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+}
